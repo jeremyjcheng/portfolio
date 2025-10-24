12155109b3949db6030a1d1772619e5d5cd2e8c6
Jeremy Cheng <jeremy.jj.cheng@gmail.com>
2025-10-23 19:39:21 -0700

    Added project count to heading

--- projects/projects.js
+++ projects/projects.js
@@ -6,5 +6,13 @@ const projects = await fetchJSON("../lib/projects.json");
 // Render the projects into the container
 const projectsContainer = document.querySelector(".projects");
 
+// Select the title element
+const titleElement = document.querySelector(".projects-title");
+
+// Displays the count of projects in the title
+titleElement.textContent = `${projects.length} Total Projects`;
+
+console.log("count of projects:", projects.length);
+
 // Call the renderProjects function to display the projects
 renderProjects(projects, projectsContainer, "h2");
