27c94cb8266bf0872cede8e6b4a974943694b5c7
Jeremy Cheng <jeremy.jj.cheng@gmail.com>
2025-10-30 15:55:13 -0700

    Fix pie chart display

--- projects/projects.js
+++ projects/projects.js
@@ -20,43 +20,8 @@ renderProjects(projects, projectsContainer, "h2");
 
 // d3 setup
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-
-// derive pie data: count of projects per year (label = year)
-let rolledData = d3
-  .rollups(
-    projects,
-    (v) => v.length,
-    (d) => d.year
-  )
-  .sort((a, b) => d3.ascending(a[0], b[0]));
-
-let data = rolledData.map(([year, count]) => {
-  return { value: count, label: year };
-});
-
-// generate pie slice data automatically (use the numeric `value` field)
 let pieGenerator = d3.pie().value((d) => d.value);
-let arcData = pieGenerator(data);
-
-// pick colors
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
-
-// append each slice to the specific projects chart svg
-d3.select("#projects-plot")
-  .selectAll("path")
-  .data(arcData)
-  .join("path")
-  .attr("d", arcGenerator)
-  .attr("fill", (d, i) => colors(i));
-
-let legend = d3.select(".legend");
-data.forEach((d, idx) => {
-  legend
-    .append("li")
-    .attr("class", "legend-item")
-    .attr("style", `--color:${colors(idx)}`) // set the style attribute while passing in parameters
-    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
-});
 
 // Search
 let query = "";
@@ -112,8 +77,7 @@ function renderPieChart(projectsGiven) {
     .join("path")
     .attr("d", newArc)
     .attr("fill", (d, i) => d3.schemeTableau10[i % 10])
-    .attr("stroke", "white")
-    .attr("transform", "translate(100,100)");
+    .attr("stroke", "white");
 
   let legend = d3.select(".legend");
   legend
